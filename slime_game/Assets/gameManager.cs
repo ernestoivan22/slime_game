4eec13c116a3de935a0493cec77a1370b0dd126a
cesarluis93 <IngCienComp93>
2016-03-02 18:10:50 -0600

    Cambio de punto de sleep

--- slime_game/Assets/gameManager.cs
+++ slime_game/Assets/gameManager.cs
@@ -25,16 +25,14 @@ public class gameManager : MonoBehaviour {
 				p1Scored = false;
 				p1Won = 1;
 				PlayerPrefs.SetInt("p1Won",p1Won);
-				if (PlayerPrefs.GetInt ("esHost") == 1) {
+//				if (PlayerPrefs.GetInt ("esHost") == 1) {
 					System.Threading.Thread.Sleep(60);
-				}
+//				}
 				Application.LoadLevel (2);
 			}
 			else{
 				p1Scored = true;
-				if (PlayerPrefs.GetInt ("esHost") == 1) {
-					System.Threading.Thread.Sleep(60);
-				}
+				System.Threading.Thread.Sleep(60);
 				Application.LoadLevel(1);
 			}
 		}
@@ -47,16 +45,12 @@ public class gameManager : MonoBehaviour {
 				p1Scored = false;
 				p1Won = 0;
 				PlayerPrefs.SetInt("p1Won",p1Won);
-				if (PlayerPrefs.GetInt ("esHost") == 1) {
-					System.Threading.Thread.Sleep(60);
-				}
+				System.Threading.Thread.Sleep(60);
 				Application.LoadLevel(2);
 			}
 			else{
 				p1Scored = false;
-				if (PlayerPrefs.GetInt ("esHost") == 1) {
-					System.Threading.Thread.Sleep(60);
-				}
+				System.Threading.Thread.Sleep(60);
 				Application.LoadLevel(1);
 			}
 
